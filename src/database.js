2fec79be5064b05b065ae46eb944950bbc8d5769
suptoasty <jason.lonsinger@gmail.com>
2020-09-07 00:00:30 -0500

    now uses mysql pools

--- src/database.js
+++ src/database.js
@@ -2,45 +2,33 @@ const mysql = require('mysql');
 const { database } = require('../config');
 
 // the database we are going to use
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: database.hostname,
     user: database.username,
     password: database.password,
     database: database.database,
 });
 
-db.connect((err)=> {
-    if(err){
-        console.log('Error connecting to Db');
-        return;
-      }
-      console.log('Connection established');
-});
-
-db.query('SELECT * FROM test', (err, rows) => {
-    if(err) throw err;
-    console.log("DATA");
-    console.log(rows);
-});
-
-db.end((err)=> {
-    if(err) {
-        console.log("Error Ending Connection");
-        return;
-    }
-    console.log("Ending Connection");
-});
-    
-// create default database
-// async function createDefaultDatabase(name = 'testdb') {
-//     let conn = dbPool.getConnection();
-    
-//     let sql = `CREATE DATABASE [IF NOT EXISTS] ` + name + `;`;
-//     await conn.query(sql);
-// };
-// createDefaultDatabase();
+function createDB(name = "testdb") {
+    let sql = 'CREATE DATABASE IF NOT EXISTS '+ name +';';
+    db.getConnection((err, connection) => {
+        if(err) {
+            callback(err);
+            return;
+        }
+        
+        connection.query(sql, (err, results) => {
+            connection.release();
+            if(err) throw err;
+        });
+        connection.on('error', (err) => {
+            callback(true);
+            return;
+        });
+    });
+}
 
 module.exports = {
     db: db,
-    // createDB: createDefaultDatabase,
+    createDB: createDB,
 }
