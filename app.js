bc2baa4dbf9e8da23f1a1cdfadd2b2637d2c1217
Jason Lonsinger <jason.lonsinger@gmail.com>
2020-09-10 11:46:48 -0500

    Changed to use const over global scope var

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
 const { enableDebug } = require('./config');
 
-var { indexRouter } = require('./routes/index');
-var { usersRouter } = require('./routes/users');
-var { coursesRouter } = require('./routes/courses');
-var { debugRouter } = require('./routes/debug');
+const { indexRouter } = require('./routes/index');
+const { usersRouter } = require('./routes/users');
+const { coursesRouter } = require('./routes/courses');
+const { debugRouter } = require('./routes/debug');
 
 
 const bodyParser = require('body-parser');
