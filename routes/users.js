4246d1838418852b5d916645479d01cc4a4bfe44
suptoasty <jason.lonsinger@gmail.com>
2020-09-30 09:51:00 -0500

    Added sample post to user login and course-plan.js routes

--- routes/users.js
+++ routes/users.js
@@ -1,47 +1,63 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { db } = require('../src/database');
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+const { db } = require("../src/database");
 
 // log a user in
-router.post('/login', function(req, res, next) {
+router.post("/login", async (req, res, next) => {
   // change to allow for session token in database
-  // ensure password is immediatly encrypted
-  
-  let userName = req.body.userName;
-  let userPassword = req.body.userPassword;
-  
-  /* 
-    will need to change to await on server response
-    then return error code or success
-  */
-  if(userName && userPassword) {
-    // SQL and further validation here
+  // ensure password is immediatly encrypted ??? if time
 
-    res.send('End of login');
-    res.end();
+  let user = req.body;
+
+  let errorMessage = await validate(user); //validate request here
+  if (errorMessage.length > 2) {
+    res.status(406);
+    res.send(errorMessage);
   } else {
+    let sql = "INSERT INTO courses.users SET ?;";
 
-    res.send("Please enter Usernam and Password");
-    res.end();
+    try {
+      db.query(sql, [user], function (err, result) {
+        if (err) {
+          res.status(500);
+          res.send(err);
+        } else {
+          res.json({ id: result.insertId });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500);
+      res.send(error);
+    }
   }
-
 });
 
 // log current user out
-router.post('/logout', function(req, res, next) {
-  res.end('post at logout');
+router.post("/logout", function (req, res, next) {
+  res.end("post at logout");
 });
 
 // create a new user
-router.post('/', function(req, res, next) {
-  res.end('post at users');
+router.post("/", function (req, res, next) {
+  res.end("post at users");
 });
+
+// validate request here...returns error message
+function validate(user) {
+  let errorMessage = "[";
+
+  if (!user.userName) {
+    errorMessage += "Invalid User Name!\n";
+  }
+
+  if (!user.userPassword) {
+    errorMessage += "Invalid User Password!";
+  }
+
+  errorMessage += "]";
+  return errorMessage;
+}
 
 module.exports = {
   usersRouter: router,
