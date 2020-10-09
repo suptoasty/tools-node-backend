7bdbde4600819b15de2b36b0a2c52021eb322cd4
suptoasty <jason.lonsinger@gmail.com>
2020-10-09 09:20:03 -0500

    Added comments for auth

--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,18 @@ const { db } = require("../src/database");
 const config = require("../config/config");
 
 router.post("/login", (req, res, next) => {
+  //verify req
+  //verify user with gauth-library
+  //find user
+  //create new token for our database
+  //create session
+
   res.json(req.body);
 });
 
 router.post("/logout", (req, res, next) => {
+  //delete session
+  //delete token from database
   res.json(req.body);
 });
 
