e59248481c22833fbd3233455b886f623b03b926
Nathan Woodruff <nathan.woodruff@eagles.oc.edu>
2020-10-14 19:51:38 -0500

    Start course_plan and course_plan_item CRUD

--- routes/course-plan.js
+++ routes/course-plan.js
@@ -23,9 +23,28 @@ router.get("/", function (req, res, next) {
   }
 });
 
-// GET courseplans for a student?
+// GET courseplans by student_id
+router.get("/student/:id", async (req, res, next) => {
+  let id = req.params.id;
+  let sql = "SELECT * FROM course_plan WHERE student = ?;";
 
-// GET courseplan
+  try {
+    db.query(sql, [id], function (err, result) {
+      if (err) {
+        res.status(500);
+        res.send(err);
+      } else {
+        res.json(result);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+    res.send(error);
+  }
+});
+
+// GET courseplan by course_plan_id
 router.get("/:id", async (req, res, next) => {
   let id = req.params.id;
   let sql = "SELECT * FROM course_plan WHERE course_plan_id = ?;";
@@ -126,6 +145,93 @@ router.put("/:id", async (req, res, next) => {
   }
 });
 
+
+
+
+// GET all course plan items for a course plan
+router.get('/:id/items', async (req, res, next) => {
+  let id = req.params.id;
+  let sql = "SELECT * FROM course_plan_item WHERE plan = ?;";
+
+  try {
+    db.query(sql, [id], function (err, result) {
+      if (err) {
+        res.status(500);
+        res.send(err);
+      } else {
+        res.json(result);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+    res.send(error);
+  }
+});
+
+// GET specific course plan item
+router.get('/items/:item_id', async (req, res, next) => {
+  let id = req.params.id;
+  let item_id = req.params.item_id;
+  let sql = "SELECT * FROM course_plan_item WHERE course_plan_item_id = ?;";
+
+  try {
+    db.query(sql, [item_id], function (err, result) {
+      if (err) {
+        res.status(500);
+        res.send(err);
+      } else {
+        res.json(result);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+    res.send(error);
+  }
+});
+
+// PUT course plan item
+router.put(':id/items/:item_id', async (req, res, next) => {
+  let course_plan = req.body;
+  course_plan.plan = req.params.id;
+  course_plan.course_plan_item_id = req.params.item_id;
+  console.log(course_plan);
+
+  let errorMessage = validate(course_plan); //validate request here
+  if (errorMessage.length > 2) {
+    res.status(406);
+    res.send(errorMessage);
+  } else {
+    let sql = "UPDATE course_plan_item SET ? WHERE course_plan_id = ?";
+
+    try {
+      db.query(sql, [course_plan, req.params.item_id], function (err, result) {
+        if (err) {
+          res.status(500);
+          res.send(err);
+        } else {
+          res.json(result);
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500);
+      res.send(error);
+    }
+  }
+});
+
+// POST course plan item
+router.post(':id/items');
+
+// DELETE course plan item
+router.delete(':id/items/:item_id');
+
+
+
+
+
 // validate request here...returns error message
 function validate(course) {
   var errorMessage = "[";
@@ -135,6 +241,10 @@ function validate(course) {
 
   errorMessage += "]";
   return errorMessage;
+}
+
+function validate_item(cp_item) {
+
 }
 
 module.exports = {
